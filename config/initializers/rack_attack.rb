25622c2b481e58f7cbbbb31182570cfdb67f829a
Wellington <wjas.ma@gmail.com>
2021-09-17 21:48:54 -0300

    config rack-attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -3,7 +3,7 @@ class Rack::Attack
 
   # Allow all local traffic  
   safelist('allow-localhost') do |req|  
-    '127.0.0.1' == req.ip || '::1' == req.ip  
+    '127.0.0.1' == req.ip || '::1' == req.ip
   end
   
   # Allow an IP address to make 10 requests every 10 seconds  
